84ab656c0e748831b30749d9ce1ada94c0ea56b1
Ryo Nihei <nihei.dev@gmail.com>
2019-11-30 19:32:56 +0900

    Simplify package structre

--- cmd/felipe/felipe.go
+++ cmd/felipe/felipe.go
@@ -3,15 +3,17 @@ package main
 import (
 	"os"
 
-	"github.com/nihei9/felipe/pkg/felipe/cmd"
+	"github.com/nihei9/felipe/cmd/felipe/dot"
+	"github.com/nihei9/felipe/cmd/felipe/query"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	os.Exit(run())
+	os.Exit(doMain())
 }
 
-func run() int {
-	cmd := cmd.NewCmd()
+func doMain() int {
+	cmd := newCmd()
 	cmd.SetOutput(os.Stdout)
 	err := cmd.Execute()
 	if err != nil {
@@ -21,4 +23,19 @@ func run() int {
 	}
 
 	return 0
+}
+
+func newCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "felipe",
+		Short:         "felipe visualizes dependencies.",
+		Long:          "felipe visualizes dependencies.",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+	}
+
+	cmd.AddCommand(query.NewCmd())
+	cmd.AddCommand(dot.NewCmd())
+
+	return cmd
 }
